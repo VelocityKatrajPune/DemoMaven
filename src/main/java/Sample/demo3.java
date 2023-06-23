d9c3f29d62c0c035d8ef04579d48d02a1d2e33ce
yogendra.joshi <yjoshi02@gmail.com>
2023-06-23 15:48:24 +0530

    [Sanjay]: new class demo3 created

---
+++ src/main/java/Sample/demo3.java
@@ -0,0 +1,23 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class demo3
+{
+	
+	@Test
+	public void TC1() throws InterruptedException
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		
+		Thread.sleep(2000);
+		
+		driver.quit();
+		
+		Thread.sleep(2000);
+	}
+
+}
